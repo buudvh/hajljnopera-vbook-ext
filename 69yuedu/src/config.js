3c09d39c7e76c2813091a9465a863ead12634fdf
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-09 13:51:27 +0700

    fix ext 69yuedu

--- 69yuedu/src/config.js
+++ 69yuedu/src/config.js
@@ -1,4 +1,5 @@
-let BASE_URL = 'https://www.69yuedu.net';
+// let BASE_URL = 'https://www.69yuedu.net';
+let BASE_URL = 'https://www.69shu.pro';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
