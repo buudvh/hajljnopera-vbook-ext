f981e726ca80a6aa985c94d6fab18698f7de4dea
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-23 11:39:11 +0700

    fix bookqq

--- bookqq/src/toc.js
+++ bookqq/src/toc.js
@@ -1,23 +1,28 @@
 function execute(url) {
     let bid = url.match(/\d+/)[0];
-    let response = fetch('https://book.qq.com/api/book/detail/chapters', {
-        method: "GET",
-        queries: {
-            bid : bid,
-        }
-    });
+    let response = fetch('https://book.qq.com/book-detail/'+bid);
     if (response.ok) {
-        let json = response.json().data;
+        // let json = response.json().data;
+        // let novelList = [];
+        // json.forEach(e => {
+        //     if(e.free != 0) let stt = '【Free】 ';
+        //     else stt = '【VIP】 ';
+        //     novelList.push({
+        //         name: stt + e.chapterName,
+        //         url: url+'/'+e.cid,
+        //         host: "http://book.qq.com"
+        //     })
+        // });
+        // return Response.success(novelList);
         let novelList = [];
-        json.forEach(e => {
-            if(e.free != 0) let stt = '【Free】 ';
-            else stt = '【VIP】 ';
+        doc.select(".list-a a").forEach(e => {
             novelList.push({
-                name: stt + e.chapterName,
-                url: url+'/'+e.cid,
+                name: e.select("span").text(),
+                url: e.attr("href"),
                 host: "http://book.qq.com"
             })
-        });
+        })
+
         return Response.success(novelList);
     }
     return null;
