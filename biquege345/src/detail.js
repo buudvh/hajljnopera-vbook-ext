2987ad4b71d6bbbdbc4436698302fdcbc04213b6
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-24 12:30:25 +0700

    mod detail

--- biquege345/src/detail.js
+++ biquege345/src/detail.js
@@ -4,18 +4,17 @@ function execute(url) {
     if (response.ok) {
 
         let doc = response.html();
-        let coverImg = doc.select(".imgbox img").first().attr("src");
+        let coverImg = doc.select(".zhutu img").first().attr("src");
         if (coverImg.startsWith("/")) {
             coverImg = "http://www.biquge345.com" + coverImg;
         }
-        let author = doc.select(".info p").first().text().replace(/作\s*者：/g, "")
-        console.log(doc.select(".desc").first().text())
+        let author = doc.select("span.x1 a").first().text();
         return Response.success({
-            name: doc.select(".info h1").text(),
+            name: doc.select(".right_border h1").text(),
             cover: coverImg,
             author: author,
-            description: doc.select(".desc").first().text().replace(/�/g, ""),
-            detail: "作者：" +  author + "<br>" + doc.select("p.xs-show").first().text()+"<br>" + doc.select(".info p").get(4).text(),
+            description: doc.select("span.x3").first().text(),
+            detail: '',
             host: "http://www.biquge345.com"
         });
     }
