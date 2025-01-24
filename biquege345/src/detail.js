ffc94d957b7548ec2ffc6005cfaa6bf74ba7b309
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-24 12:49:10 +0700

    test

--- biquege345/src/detail.js
+++ biquege345/src/detail.js
@@ -3,17 +3,25 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
 
-        let doc = response.html();
-        let coverImg = doc.select(".zhutu img").first().attr("src");
-        if (coverImg.startsWith("/")) {
-            coverImg = "http://www.biquge345.com" + coverImg;
-        }
-        let author = doc.select("span.x1 a").first().text();
+        // let doc = response.html();
+        // let coverImg = doc.select(".zhutu img").first().attr("src");
+        // if (coverImg.startsWith("/")) {
+        //     coverImg = "http://www.biquge345.com" + coverImg;
+        // }
+        // let author = doc.select("span.x1 a").first().text();
+        // return Response.success({
+        //     name: doc.select(".right_border h1").text(),
+        //     cover: coverImg,
+        //     author: author,
+        //     description: doc.select("span.x3").first().text(),
+        //     detail: '',
+        //     host: "http://www.biquge345.com"
+        // });
         return Response.success({
-            name: doc.select(".right_border h1").text(),
-            cover: coverImg,
-            author: author,
-            description: doc.select("span.x3").first().text(),
+            name: 'test',
+            cover: 'test',
+            author: 'test',
+            description: 'test',
             detail: '',
             host: "http://www.biquge345.com"
         });
