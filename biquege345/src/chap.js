ecabd40a3147fe73263ecd7e3d0525ee322b62a4
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-24 14:02:51 +0700

    mod biquge345.com

--- biquege345/src/chap.js
+++ biquege345/src/chap.js
@@ -4,13 +4,30 @@ function execute(url) {
     let data = "";
     if (response.ok) {
         let doc = response.html();
-        data = doc.select(".txt").first().text()
-                    .replace('\n','<br>')
-                    .replace(/^第\d+章.*?<br>/, '');
+        let html = cleanHtml(doc.select(".txt").first().html())
+                            .replace(/<p>(.*?)<\/p>/g, '$1<br>')
+                            .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
+                            .replace('(本章完)', '')
+        data = html;
     }
 
     if (data !== null && data !== '') 
         return Response.success(data);
     
     return null;
+}
+
+function cleanHtml(html) {
+    //remove ads
+    html = html.replace('<p style="font-weight:bold" ;="">一秒记住【笔趣阁小说网】biquge345.com，更新快，无弹窗！</p>', '');
+    // trim br tags
+    html = html.replace(/(^(<br>\s*)+|(<br>\s*)+$)/gm, '');
+    // remove duplicate br tags
+    html = html.replace(/(<br>\s*){2,}/gm, '<br>');
+    // strip html comments
+    html = html.replace(/<!--[^>]*-->/gm, '');
+    // html decode
+    html = html.replace(/&nbsp;/g, '');
+
+    return html;
 }
