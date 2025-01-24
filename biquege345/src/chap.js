f72e3d7a46465563683984f00426d99762efc3fb
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-24 13:49:02 +0700

    biquge345.com

--- biquege345/src/chap.js
+++ biquege345/src/chap.js
@@ -1,16 +1,17 @@
 function execute(url) {
-    url = url.replace('m.biquge345.com', 'www.biquge345.com');
-    let response = fetch(url);
-    let data = "";
-    if (response.ok) {
-        let doc = response.html();
-        data = doc.select(".txt").first().text()
-                    .replace('\n','<br>')
-                    .replace(/^第\d+章.*?<br>/, '');
-    }
+    // url = url.replace('m.biquge345.com', 'www.biquge345.com');
+    // let response = fetch(url);
+    // let data = "";
+    // if (response.ok) {
+    //     let doc = response.html();
+    //     data = doc.select(".txt").first().text()
+    //                 .replace('\n','<br>')
+    //                 .replace(/^第\d+章.*?<br>/, '');
+    // }
 
-    if (data !== null && data !== '') 
-        return Response.success(data);
+    // if (data !== null && data !== '') 
+    //     return Response.success(data);
     
-    return null;
+    // return null;
+    return Response.success(url);
 }
