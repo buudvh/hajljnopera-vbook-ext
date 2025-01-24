c0b796147919e7ecfb9bdf9c7ffb12bf581e7432
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-01-24 12:17:52 +0700

    mod

--- biquege345/src/gen.js
+++ biquege345/src/gen.js
@@ -5,14 +5,30 @@ function execute(url, page) {
         let doc = response.html();
         const data = [];
 
-        doc.select(".right_border").forEach(rightBorder => {
-            rightBorder.select("li").forEach(e => {
-                data.push({
-                    name: e.select("h3.p2 a").first().text() ?? e.select("span.name a").first().text(),
-                    link: e.select("h3.p2 a").first().attr("href") ?? e.select("span.name a").first().attr("href"),
-                    description: e.select("p.p3").text() ?? '',
-                    host: "http://www.biquge345.com"
-                })
+        doc.select(".right_border")[0].select("li").forEach(e => {
+            data.push({
+                name: e.select("h3.p2 a").first().text(),
+                link: e.select("h3.p2 a").first().attr("href"),
+                description: e.select("p.p3").first().text(),
+                host: "http://www.biquge345.com"
+            });
+        });
+        
+        doc.select(".right_border")[0].select("li").forEach(e => {
+            data.push({
+                name: e.select("h3.p2 a").first().text(),
+                link: e.select("h3.p2 a").first().attr("href"),
+                description: '',
+                host: "http://www.biquge345.com"
+            });
+        });
+
+        doc.select(".right_border")[2].select("li").forEach(e => {
+            data.push({
+                name: e.select("span.name a").first().text(),
+                link: e.select("span.name a").first().attr("href"),
+                description: '',
+                host: "http://www.biquge345.com"
             });
         });
 
