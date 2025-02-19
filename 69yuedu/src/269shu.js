2239a3bfd69f4d43f5e2c8dd74c12ab9a4f0a157
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-02-19 09:44:43 +0700

    mod conver image

--- 69yuedu/src/269shu.js
+++ 69yuedu/src/269shu.js
@@ -46,7 +46,7 @@ function getDetail69yuedu(url) {
         name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
         cover: host69yuedu + $.Q(doc, 'div.bookimg2 > img').attr('src'),
         author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-        description: $.Q(doc, 'div.navtxt > p').html(),
+        description: host69yuedu + $.Q(doc, 'div.bookimg2 > img').attr('src') + '\n' + $.Q(doc, 'div.navtxt > p').html(),
         detail: $.QA(doc, 'div.booknav2 p', { m: x => x.text(), j: '<br>' })
     }
     return data;
