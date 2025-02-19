0e1c17d02934794a8cfced130cdafe5ce4d400a8
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-02-19 10:02:45 +0700

    mod

--- 69shu/src/gen2.js
+++ 69shu/src/gen2.js
@@ -14,7 +14,7 @@ function execute(url, page) {
             data.push({
                 name: e.select("h3").text().trim(),
                 link: e.select("h3 a").attr('href'),
-                cover: e.select("img").attr('data-src').trim(),
+                cover: "https://static.sangtacvietcdn.xyz/img/bookcover256.jpg",
                 description: $.Q(e, '.zxzj > p').text().replace('最近章节', ''),
                 host: BASE_URL
             })
