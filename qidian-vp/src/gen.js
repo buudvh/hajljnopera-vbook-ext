a2348ce6430b0c5c655c6e8ba0a382e9d232a216
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-09 13:22:37 +0700

    mod

--- qidian-vp/src/gen.js
+++ qidian-vp/src/gen.js
@@ -7,14 +7,14 @@ function execute(url, page) {
     if (response.ok) {
         let doc = response.html('gbk');
         var data = [];
-        var elems = $.QA(doc, 'article');
+        var elems = $.QA(doc, 'a.items-center');
         if (!elems.length) return Response.error(url);
         elems.forEach(function(e) {
             data.push({
                 name: bookData.book_name,
-                link: $.QA(e, 'a').attr('href'),
-                cover: $.QA(e, 'article > a > img').attr('src'),
-                description: $.QA(e, 'article > div > div:nth-child(2)').text(),
+                link: e.attr('href'),
+                cover: $.QA(e, 'img').attr('src'),
+                description: $.QA(e, 'div > div:nth-child(2)').text(),
                 author: "",
                 kind: ""
             })
