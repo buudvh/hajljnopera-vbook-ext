e54095dc4c68cae7cbd1fff7b512bbb829dafe60
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-09 16:31:05 +0700

    mod

--- qidian-vp/src/chap.js
+++ qidian-vp/src/chap.js
@@ -6,6 +6,7 @@ function execute(url) {
         if (response.ok) {
             let doc = response.html();
             let htm = doc.select("#chapter-content").html();
+            htm.select("h2").remove();
             htm = cleanHtml(htm);
             return Response.success(htm);
         }
