4d82d5709e161a1442c6a31345bc4bd11e06e9cc
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-09 13:34:17 +0700

    mod

--- qidian-vp/src/gen.js
+++ qidian-vp/src/gen.js
@@ -1,26 +1,31 @@
 load('libs.js');
 load('gbk.js');
 function execute(url, page) {
-    if (!page) page = '1';
-    let inputUrl = url + '?page=' + page;
-    let response = fetch(inputUrl);
-    if (response.ok) {
-        let doc = response.html();
-        var data = [];
-        var elems = $.QA(doc, 'a.items-center');
-        if (!elems.length) return Response.error(url);
-        elems.forEach(function(e) {
-            data.push({
-                name: bookData.book_name,
-                link: e.attr('href'),
-                cover: $.QA(e, 'img').attr('src'),
-                description: $.QA(e, 'div > div:nth-child(2)').text(),
-                author: "",
-                kind: ""
+    try {
+        if (!page) page = '1';
+        let inputUrl = url + '?page=' + page;
+        let response = fetch(inputUrl);
+        if (response.ok) {
+            let doc = response.html();
+            var data = [];
+            var elems = $.QA(doc, 'a.items-center');
+            if (!elems.length) return Response.error(url);
+            elems.forEach(function (e) {
+                data.push({
+                    name: bookData.book_name,
+                    link: e.attr('href'),
+                    cover: $.QA(e, 'img').attr('src'),
+                    description: $.QA(e, 'div > div:nth-child(2)').text(),
+                    author: "",
+                    kind: ""
+                })
             })
-        })
-        let next_page = parseInt(page) + 1;
-        return Response.success(data, next_page.toString());
+            let next_page = parseInt(page) + 1;
+            return Response.success(data, next_page.toString());
+        }
+        return null;
+    } catch (e) { 
+        return Response.error(e);
     }
-    return null;
+
 }
