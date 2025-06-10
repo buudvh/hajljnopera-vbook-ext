feb5c1c0fefdc3f2a90b52402ec4a6d7fd69101a
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-10 17:06:27 +0700

    mod

--- qidian-vp/src/toc.js
+++ qidian-vp/src/toc.js
@@ -5,30 +5,50 @@ function execute(url) {
     charpters = [];
     temp = [];
     try {
-        do {
-            temp = [];
-            if (url.indexOf('muc-luc') == -1) url = url + '/muc-luc';
-            let inputUrl = url + '?page=' + page;
-            let response = fetch(inputUrl);
-            if (response.ok) {
-                let doc = response.html();
-                var elems = doc.select("a[id]");
-                elems.forEach(function (e) {
-                    charpters.push({
-                        name: e.select("h3").text(),
-                        url: e.attr('href'),
-                        host: "",
-                    });
-                    temp.push({
-                        name: e.select("h3").text(),
-                        url: e.attr('href'),
-                        host: "",
-                    });
-                })
-                page = parseInt(page) + 1;
-            }
+        // do {
+        //     temp = [];
+        //     if (url.indexOf('muc-luc') == -1) url = url + '/muc-luc';
+        //     let inputUrl = url + '?page=' + page;
+        //     let response = fetch(inputUrl);
+        //     if (response.ok) {
+        //         let doc = response.html();
+        //         var elems = doc.select("a[id]");
+        //         elems.forEach(function (e) {
+        //             charpters.push({
+        //                 name: e.select("h3").text(),
+        //                 url: e.attr('href'),
+        //                 host: "",
+        //             });
+        //             temp.push({
+        //                 name: e.select("h3").text(),
+        //                 url: e.attr('href'),
+        //                 host: "",
+        //             });
+        //         })
+        //         page = parseInt(page) + 1;
+        //     }
+        // }
+        // while (temp.length != 0);
+        if (url.indexOf('muc-luc') == -1) url = url + '/muc-luc';
+        let inputUrl = url + '?page=' + page;
+        let response = fetch(inputUrl);
+        if (response.ok) {
+            let doc = response.html();
+            var elems = doc.select("a[id]");
+            elems.forEach(function (e) {
+                charpters.push({
+                    name: e.select("h3").text(),
+                    url: e.attr('href'),
+                    host: "",
+                });
+                temp.push({
+                    name: e.select("h3").text(),
+                    url: e.attr('href'),
+                    host: "",
+                });
+            })
+            page = parseInt(page) + 1;
         }
-        while (temp.length != 0);
         return Response.success(charpters);
     } catch (error) {
         return Response.success([{
