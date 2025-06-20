f1f223242ce86b24353d611460a87c45790746d9
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-20 16:36:53 +0700

    mod

--- idejian/src/home.js
+++ idejian/src/home.js
@@ -12,5 +12,10 @@ function execute() {
         { title: "男-竞技", input: "/books/nansheng?categoryId=1123", script: "zen.js" },
         { title: "男-科幻", input: "/books/nansheng?categoryId=1124", script: "zen.js" },
         { title: "男-灵异", input: "/books/nansheng?categoryId=1125", script: "zen.js" },
+        { title: "人气热", input: "/bangdan/36014", script: "gen.js" },
+        { title: "点击榜", input: "/bangdan/36015", script: "gen.js" },
+        { title: "收藏榜", input: "/bangdan/36016", script: "gen.js" },
+        { title: "新书潜力", input: "/bangdan/36017", script: "gen.js" },
+        { title: "完本榜", input: "/bangdan/36021", script: "gen.js" },
     ]);
 }
