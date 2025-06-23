c36eb5c64ad0fded1604bb3e7d56963de12d58a7
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-23 09:46:31 +0700

    mod

--- 69shu/src/detail.js
+++ 69shu/src/detail.js
@@ -7,6 +7,20 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
+        let genres = [];
+        genres.push({
+            title: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
+            input: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').attr('href'),
+            script: "gen2.js"
+        })
+
+        doc.select("#tagul a").forEach(element => {
+            genres.push({
+                title: element.text().trim(),
+                input: element.attr('href'),
+                script: "gen2.js"
+            })
+        });
 
         return Response.success({
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
@@ -14,7 +28,8 @@ function execute(url) {
             author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
             description: $.Q(doc, 'div.navtxt > p').html(),
             detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
-            host: BASE_URL
+            host: BASE_URL,
+            genres: genres,
         })
     }
     return null;
