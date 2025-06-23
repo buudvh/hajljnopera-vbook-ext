a9cb680570b64f1eebe17e03a96d1d5d6de55a96
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-23 15:14:04 +0700

    mod

--- 69shu/src/detail.js
+++ 69shu/src/detail.js
@@ -8,26 +8,6 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html('gbk');
-        let genres = [];
-
-        genres.push({
-            title: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-            input: encodeAuhtorUrl($.Q(doc, 'div.booknav2 > p:nth-child(2) > a').attr("href")),
-            script: "author.js"
-        })
-
-        let script = doc.select('script').text();
-        let bookInfor = getBookInfor(script);
-        if (bookInfor) {
-            let tags = bookInfor.tags.split('|');
-            for (let i = 0; i < tags.length; i++) {
-                genres.push({
-                    title: tags[i],
-                    input: "/" + tags[i] + "/{0}/",
-                    script: "gen2.js"
-                })
-            }
-        }
         
         return Response.success({
             name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
@@ -36,10 +16,9 @@ function execute(url) {
             description: $.Q(doc, 'div.navtxt > p').html(),
             detail: $.QA(doc, 'div.booknav2 p', {m: x => x.text(), j: '<br>'}),
             host: BASE_URL,
-            genres: genres,
             suggests: [
                 {
-                    title: "同一作者",
+                    title: "同作者",
                     input: encodeAuhtorUrl($.Q(doc, 'div.booknav2 > p:nth-child(2) > a').attr("href")),
                     script: "author.js"
                 }
