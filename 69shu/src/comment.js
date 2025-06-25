4d1d97d6534d3b67e970103cfbb8af737397f0bb
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-25 10:55:34 +0700

    mod

--- 69shu/src/comment.js
+++ 69shu/src/comment.js
@@ -1,42 +1,51 @@
 load('config.js');
 function execute(bookid, next) {
     if (!next) next = "0"
-    let response = fetch("http://14.225.254.182/io/comment/webComments", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "start": next,
-            "objectid": bookid,
-            "objecttype": "69shu"
-        })
-    });
-    if (response.ok) {
-        let doc = response.html();
-        let comments = [];
-        let listCmtElm = doc.select('div.flex')
-        comments.push({
-            name: "LOG BUG",
-            content: "bookid: " + bookid + " next: " + next + " listCmtElm: " + listCmtElm.length,
+    try {
+        let response = fetch("http://14.225.254.182/io/comment/webComments", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "start": next,
+                "objectid": bookid,
+                "objecttype": "69shu"
+            })
         });
-        listCmtElm.forEach(function (e) {
-            comments.push({
-                name: e.select('div.sec-bot a').text(),
-                content: e.select('div.sec-top').text(),
-            });
-        });
+        if (response.ok) {
+            let doc = response.html();
+            let comments = [];
+            let listCmtElm = doc.select('div.flex')
 
-        var nextpage = e.select('#cmtwd').attr('data-start');
-        if (listCmtElm.length > 0) {
-            return Response.success(comments, nextpage + "");
+            comments.push({
+                name: "LOG BUG",
+                content: "bookid: " + bookid + " next: " + next + " listCmtElm: " + listCmtElm.length,
+            });
+
+            listCmtElm.forEach(function (e) {
+                comments.push({
+                    name: e.select('div.sec-bot a').text(),
+                    content: e.select('div.sec-top').text(),
+                });
+            });
+
+            var nextpage = e.select('#cmtwd').attr('data-start');
+            if (listCmtElm.length > 0) {
+                return Response.success(comments, nextpage + "");
+            }
+
+            return Response.success(comments, null);
         }
 
-        return Response.success(comments, null);
+        return Response.success([{
+            name: "LOG BUG",
+            content: "bookid: " + bookid + " next: " + next + "  status: " + response.status,
+        }], null);
+    } catch (e) {
+        return Response.success([{
+            name: "LOG BUG",
+            content: "bookid: " + bookid + " next: " + next + "  exception: " + e.message,
+        }], null);
     }
-
-    return Response.success([{
-        name: "LOG BUG",
-        content: "bookid: " + bookid + " next: " + next + "  status: " + response.status,
-    }], null);
 }
