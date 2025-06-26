fb2b14e99c0987bcc4bf84d583e51bb777de5e5b
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-06-26 17:35:51 +0700

    mod

---
+++ 69shu/src/tag.js
@@ -0,0 +1,34 @@
+load('libs.js');
+load('config.js');
+function execute(tag, page) {
+    let url = STVHOST + '/io/searchtp/searchBooks/?find=&tag=' + tag +'&sort=update&host=69shu&minc=0&tag=&p=' + page;
+    let response = fetch(url);
+    function toCapitalize(sentence) {
+        const words = sentence.split(" ");
+
+        return words.map((word) => {
+            return word[0].toUpperCase() + word.substring(1);
+        }).join(" ");
+    }
+
+    if (response.ok) {
+        let doc = response.html()
+        let next = parseInt(page, 10) + 1;
+        let el = doc.select("a.booksearch")
+        let data = [];
+        el.forEach(e => {
+            let stv_story_link = e.select("a").first().attr("href");
+            let bookid = stv_story_link.split("/")[4];
+            data.push({
+                name: toCapitalize(e.select(".searchbooktitle").first().text()),
+                link: SHU69_HOST + "book/" + bookid + ".htm",
+                cover: e.select("img").first().attr("src") || "https://static.sangtacvietcdn.xyz/img/bookcover256.jpg",
+                description: e.select(" div > span.searchtag").last().text(),
+                host: ""
+            })
+        });
+
+        return Response.success(data, next.toString());
+    }
+    return null;
+}
