578bb8d4f3d2fe8cc998c2c346fc41542c6f5964
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-08 15:10:44 +0700

    MOD: log bug biqubu3

--- biqubu3/src/detail.js
+++ biqubu3/src/detail.js
@@ -4,17 +4,30 @@ load('gbk.js');
 
 function execute(url) {
     let response = fetch(url);
-    if (response.ok) {
-        let doc = response.html();
+    try {
+        if (response.ok) {
+            let doc = response.html();
 
+            return Response.success({
+                name: $.Q(doc, '#info > h1').text(),
+                cover: doc.select("#fmimg > img").attr("src") || DEFAULT_COVER,
+                author: $.Q(doc, '#info > p:nth-child(1) >').text().trim().replace('作    者：', ''),
+                description: $.QA(doc, '#intro p', { m: x => x.text(), j: '<br>' }),
+                detail: '',
+                host: BASE_URL,
+            })
+
+        }
+        return null;
+    } catch (error) {
         return Response.success({
-            name: $.Q(doc, '#info > h1').text(),
-            cover: doc.select("#fmimg > img").attr("src") || DEFAULT_COVER,
-            author: $.Q(doc, '#info > p:nth-child(1) >').text().trim().replace('作    者：', ''),
-            description: $.QA(doc, '#intro p', { m: x => x.text(), j: '<br>' }),
-            detail: '',
-            host: BASE_URL,
-        })
+                name: 'BUG',
+                cover: DEFAULT_COVER,
+                author: '',
+                description: error.message,
+                detail: '',
+                host: BASE_URL,
+            })
     }
-    return null;
+
 }
