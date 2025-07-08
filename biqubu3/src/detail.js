f4eda21664139a4bf66546987bbb3cf55b40182b
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-08 15:00:37 +0700

    add biqubu3

---
+++ biqubu3/src/detail.js
@@ -0,0 +1,20 @@
+load('libs.js');
+load('config.js');
+load('gbk.js');
+
+function execute(url) {
+    let response = fetch(url);
+    if (response.ok) {
+        let doc = response.html('gbk');
+
+        return Response.success({
+            name: $.Q(doc, '#info > h1').text(),
+            cover: doc.select("#fmimg > img").attr("src") || DEFAULT_COVER,
+            author: $.Q(doc, '#info > p:nth-child(1) >').text().trim().replace('作    者：', ''),
+            description: $.QA(doc, '#intro p', { m: x => x.text(), j: '<br>' }),
+            detail: '',
+            host: BASE_URL,
+        })
+    }
+    return null;
+}
