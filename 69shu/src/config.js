b771e8c8d6712fcd30084365dddc8a008354105d
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-09 11:01:36 +0700

    mod

--- 69shu/src/config.js
+++ 69shu/src/config.js
@@ -1,5 +1,4 @@
-//    var host = 'https://www.69shuba.com';
-let BASE_URL = 'https://www.69shu.pro';
+let BASE_URL = 'https://www.69shuba.com';
 const STVHOST = "http://14.225.254.182";
 const SHU69_HOST = "https://www.69shuba.com/"
 const DEFAULT_COVER = "https://static.sangtacvietcdn.xyz/img/bookcover256.jpg";
