be381d992532d47c024fa7ea591aa54dfd48f6e3
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-09 10:21:20 +0700

    mod

--- 69shu/src/chap.js
+++ 69shu/src/chap.js
@@ -3,27 +3,32 @@ load('libs.js');
 function execute(url) {
     try {
         url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+
         let response = fetch(url);
+
+        let doc;
         if (response.ok) {
-            let doc = response.html('gbk');
-            if (doc.html().includes("69shubaV1")) {
-                var browser = Engine.newBrowser() // Khởi tạo browser
-                doc = browser.launch(url, 4000)
-            }
-            var htm = doc.select(".txtnav")
-            htm.select(".contentadv").remove()
-            htm.select(".bottom-ad").remove()
-            htm.select(".txtinfo").remove()
-            htm.select("#txtright").remove()
-            htm.select("h1").remove()
-            htm = htm.html()
-            htm = cleanHtml(htm)
-                .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
-                .replace('(本章完)', '')
-                ;
-            return Response.success(htm);
+            doc = response.html('gbk');
+        } else if (response.status == 403) {
+            var browser = Engine.newBrowser();
+            doc = browser.launch(url, 4000);
+        } else {
+            return Response.success(`fetch ${url} failed: status ${response.status}`);
         }
-        return Response.success(`fetch ${url} failed: status ${response.status}`);
+
+        var htm = doc.select(".txtnav")
+        htm.select(".contentadv").remove()
+        htm.select(".bottom-ad").remove()
+        htm.select(".txtinfo").remove()
+        htm.select("#txtright").remove()
+        htm.select("h1").remove()
+        htm = htm.html()
+        htm = cleanHtml(htm)
+            .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
+            .replace('(本章完)', '')
+            ;
+            
+        return Response.success(htm);
     } catch (error) {
         return Response.success(`fetch ${url} failed: ${error.message}`);
     }
