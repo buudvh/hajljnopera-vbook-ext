aedf48fd6e93229578ef9a636000c04c8ecb5fc0
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-09 13:18:35 +0700

    mod

--- 69shu/src/chap.js
+++ 69shu/src/chap.js
@@ -2,15 +2,7 @@ load('config.js');
 load('libs.js');
 function execute(url) {
     try {
-        url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
-
-        let response = fetch(url);
-
-        let doc;
-        if (response.ok) {
-            doc = response.html('gbk');
-        } else if (response.status == 403) {
-            var browser = Engine.newBrowser();
+        var browser = Engine.newBrowser();
             if(!browser) {
                 return Response.success(`fetch ${url} failed: no browser`);
             }else {
@@ -20,24 +12,42 @@ function execute(url) {
                 }
                 return Response.success(content);
             }
-            doc = browser.launch(url, 4000);
-        } else {
-            return Response.success(`fetch ${url} failed: status ${response.status}`);
-        }
 
-        var htm = doc.select(".txtnav")
-        htm.select(".contentadv").remove()
-        htm.select(".bottom-ad").remove()
-        htm.select(".txtinfo").remove()
-        htm.select("#txtright").remove()
-        htm.select("h1").remove()
-        htm = htm.html()
-        htm = cleanHtml(htm)
-            .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
-            .replace('(本章完)', '')
-            ;
+        // url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
 
-        return Response.success(htm);
+        // let response = fetch(url);
+
+        // let doc;
+        // if (response.ok) {
+        //     doc = response.html('gbk');
+        // } else if (response.status == 403) {
+        //     var browser = Engine.newBrowser();
+        //     if(!browser) {
+        //         return Response.success(`fetch ${url} failed: no browser`);
+        //     }else {
+        //         var content = ''
+        //         for (var prop in browser) {
+        //             content += 'Property:', prop, 'Type:', typeof browser[prop] + '\n';
+        //         }
+        //         return Response.success(content);
+        //     }
+        // } else {
+        //     return Response.success(`fetch ${url} failed: status ${response.status}`);
+        // }
+
+        // var htm = doc.select(".txtnav")
+        // htm.select(".contentadv").remove()
+        // htm.select(".bottom-ad").remove()
+        // htm.select(".txtinfo").remove()
+        // htm.select("#txtright").remove()
+        // htm.select("h1").remove()
+        // htm = htm.html()
+        // htm = cleanHtml(htm)
+        //     .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
+        //     .replace('(本章完)', '')
+        //     ;
+
+        // return Response.success(htm);
     } catch (error) {
         return Response.success(`fetch ${url} failed: ${error.message}`);
     }
