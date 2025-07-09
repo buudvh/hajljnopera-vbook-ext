7e007e32902d6204530009b370876fff364acfb3
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-09 13:24:29 +0700

    mod

--- 69shu/src/chap.js
+++ 69shu/src/chap.js
@@ -2,52 +2,28 @@ load('config.js');
 load('libs.js');
 function execute(url) {
     try {
-        var browser = Engine.newBrowser();
-            if(!browser) {
-                return Response.success(`fetch ${url} failed: no browser`);
-            }else {
-                var content = ''
-                for (var prop in browser) {
-                    content += 'Property:', prop, 'Type:', typeof browser[prop] + '\n';
-                }
-                return Response.success(content);
-            }
+        url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
 
-        // url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+        let response = fetch(url);
 
-        // let response = fetch(url);
+        if (!response.ok) {
+            return Response.success(`fetch ${url} failed: status ${response.status}`);
+        }
 
-        // let doc;
-        // if (response.ok) {
-        //     doc = response.html('gbk');
-        // } else if (response.status == 403) {
-        //     var browser = Engine.newBrowser();
-        //     if(!browser) {
-        //         return Response.success(`fetch ${url} failed: no browser`);
-        //     }else {
-        //         var content = ''
-        //         for (var prop in browser) {
-        //             content += 'Property:', prop, 'Type:', typeof browser[prop] + '\n';
-        //         }
-        //         return Response.success(content);
-        //     }
-        // } else {
-        //     return Response.success(`fetch ${url} failed: status ${response.status}`);
-        // }
+        let doc = response.html('gbk');
+        var htm = doc.select(".txtnav")
+        htm.select(".contentadv").remove()
+        htm.select(".bottom-ad").remove()
+        htm.select(".txtinfo").remove()
+        htm.select("#txtright").remove()
+        htm.select("h1").remove()
+        htm = htm.html()
+        htm = cleanHtml(htm)
+            .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
+            .replace('(本章完)', '')
+            ;
 
-        // var htm = doc.select(".txtnav")
-        // htm.select(".contentadv").remove()
-        // htm.select(".bottom-ad").remove()
-        // htm.select(".txtinfo").remove()
-        // htm.select("#txtright").remove()
-        // htm.select("h1").remove()
-        // htm = htm.html()
-        // htm = cleanHtml(htm)
-        //     .replace(/^第\d+章.*?<br>/, '') // Ex: '  第11745章 大结局，终<br>'
-        //     .replace('(本章完)', '')
-        //     ;
-
-        // return Response.success(htm);
+        return Response.success(htm);
     } catch (error) {
         return Response.success(`fetch ${url} failed: ${error.message}`);
     }
