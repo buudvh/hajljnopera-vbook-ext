9ab52a3aff2094fa61af58f889707cb2c03ef99c
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-11 13:42:30 +0700

    update tag detail 69shu

--- 69shuba/src/detail.js
+++ 69shuba/src/detail.js
@@ -16,71 +16,80 @@ function execute(url) {
             bookid = match[1];
         }
 
-        let response = fetch(url);
-        if (response.ok) {
-            let doc = response.html('gbk');
+        // let response = fetch(url);
+        // if (!response.ok) {
+        //     return Response.error(`fetch ${url} failed: status ${response.status}`);
+        // }
 
-            let genres = [];
-            genres.push({
-                title: $.Q(doc, 'div.booknav2 > p:nth-child(3) > a').text().trim() || "没有标签",
-                input: $.Q(doc, 'div.booknav2 > p:nth-child(3) > a').attr("href") || "https://www.69shuba.com/novels/class/0.htm",
-                script: "classify.js"
-            });
+        // let doc = response.html('gbk');
 
-            let comments = [];
+        var browser = Engine.newBrowser(); // Khởi tạo browser
+        browser.setUserAgent(UserAgent.android()); // Tùy chỉnh user agent
+        browser.launch(url, 3000); // Mở trang web với timeout, trả về Document object
+        let doc = browser.callJs(createDocInforDiv, 1000); // Gọi Javascript function trên trang với waitTime, trả về Document object
 
-            comments.push({
-                title: "评论",
-                input: bookid,
-                script: "comment.js"
-            });
+        let genres = [];
+        genres.push({
+            title: $.Q(doc, 'div.booknav2 > p:nth-child(3) > a').text().trim() || "没有标签",
+            input: $.Q(doc, 'div.booknav2 > p:nth-child(3) > a').attr("href") || "https://www.69shuba.com/novels/class/0.htm",
+            script: "classify.js"
+        });
 
-            return Response.success({
-                name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
-                cover: doc.select("div.bookimg2 > img").attr("src") || DEFAULT_COVER,
-                author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
-                description: $.Q(doc, 'div.navtxt > p').html(),
-                detail: $.QA(doc, 'div.booknav2 p', { m: x => x.text(), j: '<br>' }) + '<br>' + '书籍编号: ' + bookid + '<br>',
-                host: BASE_URL,
-                suggests: [
-                    {
-                        title: "同作者",
-                        input: encodeAuhtorUrl($.Q(doc, 'div.booknav2 > p:nth-child(2) > a').attr("href")),
-                        script: "author.js"
-                    }
-                ],
-                genres: genres,
-                comments: comments
+        let objBookInfor = getBookInfor(doc);
+        if (objBookInfor?.tags) {
+            tags = objBookInfor.tags.split("|")
+            tags.forEach(function (tag) {
+                genres.push({
+                    title: tag,
+                    input: `/${tag}/{0}/`, 
+                    script: "gen2.js"
+                });
             })
         }
 
-        return Response.error(`fetch ${url} failed: status ${response.status}`);
+        let comments = [];
+
+        comments.push({
+            title: "评论",
+            input: bookid,
+            script: "comment.js"
+        });
+
+        return Response.success({
+            name: $.Q(doc, 'div.booknav2 > h1 > a').text(),
+            cover: doc.select("div.bookimg2 > img").attr("src") || DEFAULT_COVER,
+            author: $.Q(doc, 'div.booknav2 > p:nth-child(2) > a').text().trim(),
+            description: $.Q(doc, 'div.navtxt > p').html(),
+            detail: $.QA(doc, 'div.booknav2 p', { m: x => x.text(), j: '<br>' }) + '<br>' + '书籍编号: ' + bookid + '<br>',
+            host: BASE_URL,
+            suggests: [
+                {
+                    title: "同作者",
+                    input: encodeAuhtorUrl($.Q(doc, 'div.booknav2 > p:nth-child(2) > a').attr("href")),
+                    script: "author.js"
+                }
+            ],
+            genres: genres,
+            comments: comments
+        })
     } catch (error) {
         return Response.error(`fetch ${url} failed: ${error.message}`);
     }
+}
+
+function getBookInfor(doc) {
+    return JSON.parse(doc.select("#div-book-infor").attr('book-infor'))
+}
+
+function createDocInforDiv() {
+    const divTag = document.createElement('div');
+    divTag.setAttribute('book-infor', JSON.stringify(bookinfo))
+    divTag.id = "div-book-infor"
+    document.body.append(divTag)
 }
 
 function encodeAuhtorUrl(url) {
     const baseUrl = "https://www.69shuba.com/modules/article/author.php?author=";
     let author = GBK.encode(url.replace(baseUrl, ""));
     return baseUrl + author;
-}
-
-function getBookInfor(doc) {
-    let start = doc.indexOf('var bookinfo =');
-    if (start == -1) return null;
-    const slice = html.slice(start);
-
-    let bookinfoText = slice
-        .replace(/^.*?=\s*/, '')       // bỏ phần "var bookinfo ="
-        .replace(/;\s*$/, '')          // bỏ dấu ; ở cuối
-        .split('</script>')[0];        // cắt nếu còn phần sau
-
-    // Ép object thành JSON bằng replace
-    bookinfoText = bookinfoText
-        .replace(/(\w+):/g, '"$1":')   // key: → "key":
-        .replace(/,\s*}/g, '}')        // bỏ dấu phẩy cuối
-        .replace(/,\s*]/g, ']');
-
-    return JSON.parse(bookinfoText);
 }
