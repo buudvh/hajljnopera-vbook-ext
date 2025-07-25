63b764cebe0f516af52e56f59217d2d2915999f4
Dam Van Hoang Buu <buudvh@glotechvn.com>
2025-07-25 09:37:33 +0700

    mod

--- 69shuba/src/toc.js
+++ 69shuba/src/toc.js
@@ -54,46 +54,45 @@ function formatName(name) {
 }
 
 function trySTV(url) {
-    const result = [];
-    let isSTV = url.indexOf("sangtacviet") !== -1 || url.indexOf("14.225.254.182") !== -1;
-    const book_id = extractBookId(url, isSTV);
-    url = `${STVHOST}/index.php?ngmar=chapterlist&h=69shu&bookid=${book_id}&sajax=getchapterlist`;
-    let response = fetch(url);
+    try {
+        const result = [];
+        let isSTV = url.indexOf("sangtacviet") !== -1 || url.indexOf("14.225.254.182") !== -1;
+        const book_id = extractBookId(url, isSTV);
+        url = `${STVHOST}/index.php?ngmar=chapterlist&h=69shu&bookid=${book_id}&sajax=getchapterlist`;
+        let response = fetch(url);
 
-    // if (!response.ok) return Response.error(`fetch ${url} failed: status ${response.status}`);
-    if (!response.ok) return Response.success([{
-        name: `fetch ${url} failed: status ${response.status}`,
-        url: "",
-        host: "",
-        id: ""
-    }]);
+        if (!response.ok) return Response.success([{
+            name: `fetch ${url} failed: status ${response.status}`,
+            url: "",
+            host: "",
+            id: ""
+        }]);
 
 
-    let objData = JSON.parse(response.text())
+        let objData = JSON.parse(response.text())
 
-    if (objData.code != '1') return Response.success([{
-        name: `fetch ${url} failed: x.code = ${objData.code}`,
-        url: "",
-        host: "",
-        id: ""
-    }]);
+        if (objData.code != '1') return Response.success([{
+            name: `fetch ${url} failed: x.code = ${objData.code}`,
+            url: "",
+            host: "",
+            id: ""
+        }]);
 
-    const chapters = objData.data.split("-//-");
+        const chapters = objData.data.split("-//-");
 
-    for (let i = 0; i < chapters.length; i++) {
-        const [_, chapterId, chapterName] = chapters[i].split("-/-");
-        result.push({
-            chapterId: parseInt(chapterId),
-            chapterName: chapterName.trim().replace(/([\t\n]+|<br>| )/g, "")
-        });
+        for (let i = 0; i < chapters.length; i++) {
+            const [_, chapterId, chapterName] = chapters[i].split("-/-");
+            result.push({
+                name: chapterName.trim().replace(/([\t\n]+|<br>| )/g, ""),
+                url: `${BASE_URL}/txt/${book_id}/${chapterId}`,
+                host: ``,
+                id: chapterId
+            })
+        }
 
-        result.push({
-            name: chapterName.trim().replace(/([\t\n]+|<br>| )/g, ""),
-            url: `${BASE_URL}/txt/${book_id}/${chapterId}`,
-            host: ``,
-            id: chapterId
-        })
+        return Response.success(result);
+    } catch (error) {
+        throw error;
     }
 
-    return Response.success(result);
 }
